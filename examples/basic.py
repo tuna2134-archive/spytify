1644a0a1c4c4e63f9e39f0dddc84e517609437d6
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-11 19:12:40 +0900

    Update basic.py

--- examples/basic.py
+++ examples/basic.py
@@ -9,7 +9,7 @@ async def main():
     await client.get_token(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
     album = await client.fetch_album("6PRPWkHY4EfDWMt5mK0jut")
     m3u = []
-    for item in (await album.fetch_tracks(limit=1)).items:
+    for item in (await album.fetch_tracks(limit=10)).items:
         m3u.append(item.uri)
     with open("music.m3u", "a") as f:
         f.write("\n".join(m for m in m3u))
