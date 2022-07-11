1a9b95204d6425a222096a0bedf782f808dd4b28
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-11 20:54:37 +0900

    Update basic.py

--- examples/basic.py
+++ examples/basic.py
@@ -8,6 +8,8 @@ client = Client()
 async def main():
     await client.get_token(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
     album = await client.fetch_album("6PRPWkHY4EfDWMt5mK0jut")
+    user = await client.get_user()
+    print(user.email)
     fp = open("playlist.m3u", 'a')
     for item in (await album.fetch_tracks(limit=10)).items:
         fp.write("{}\n".format(item.uri))
