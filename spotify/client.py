ed3770e79c78807de45d6031e1bfbe8d4ba37315
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-11 09:35:47 +0000

    ok

--- spotify/client.py
+++ spotify/client.py
@@ -4,19 +4,45 @@ from .albums import Album
 
 
 class Client:
+    """
+    Client class for Spotify api
+
+    Attributes:
+        requester (SpotifyRequester): http requester
+    """
+
     def __init__(self):
         self.requester = SpotifyRequester()
 
     async def get_token(self, client_id: str, client_secret: str) -> Account:
+        """
+        Get a token. This can take access_token
+        
+        Args:
+            cleint_id (str): cleint id
+            client_secret (str): client secret
+
+        Returns:
+            Account: account object"""
         return Account(await self.requester.get_token(client_id, client_secret))
 
     async def close(self) -> None:
+        "Close the client."
         await self.requester.close()
 
     async def fetch_album(
         self, album_id: str, *, limit: int | None = 20, store: str | None = None,
         offset: int | None = None
     ) -> Album:
+        """
+        Fetch an album.
+
+        Args:
+            album_id (str): album id
+            limit (int | None): limit
+            store (str | None): store
+            offset (int | None): offset
+        """
         payload = {}
         if limit is not None:
             payload["limit"] = limit
