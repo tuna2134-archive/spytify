e7e53e090542bb26a8ace0b9bb32a54527ffc4ad
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-11 19:15:09 +0900

    Update basic.py

--- examples/basic.py
+++ examples/basic.py
@@ -8,11 +8,10 @@ client = Client()
 async def main():
     await client.get_token(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
     album = await client.fetch_album("6PRPWkHY4EfDWMt5mK0jut")
-    m3u = []
+    fp = open("playlist.m3u", 'a')
     for item in (await album.fetch_tracks(limit=10)).items:
-        m3u.append(item.uri)
-    with open("music.m3u", "a") as f:
-        f.write("\n".join(m for m in m3u))
+        fp.write("{}\n".format(item.uri))
+    fp.close()
     await client.close()
 
 asyncio.run(main())
